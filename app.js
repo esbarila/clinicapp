dbf7c16164d136ba9df88cd16c6f3b8fe94e31d6
Esteban Barila <esteban.barila@gmail.com>
2021-09-27 22:51:06 -0300

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
 
+let routes = require('./routes');
 let app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(routes);
 app.listen(app.get('port'), function(){
     console.log('Server started on port ' + app.get('port'));
 });
